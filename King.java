b2e7e5e95408b0b9826dccfe09c98ef26835f226
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2019-01-15 17:35:44 +0100

    init king class

---
+++ King.java
@@ -0,0 +1,17 @@
+public class King {
+
+  boolean type;       //Black or white(true)
+  boolean alive;      //It is alive
+  int[] pos = {0,0};  //Its position
+
+  King(boolean white) {
+
+  }
+
+  //Direction is a int 0-7. 0 is forward, 1 is diagonal right and so...
+  //Amount is the
+  void move(int dir, int amo) {
+
+  }
+
+}
