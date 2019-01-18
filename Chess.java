b7454afafa687c88ce16e434d1ab95d2b4b4c774
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2019-01-18 09:52:08 +0100

    Colors

--- Chess.java
+++ Chess.java
@@ -1,12 +1,37 @@
 public class Chess {
 
+  static final String RESET    = "\033[0m";   //Nulstil
+  final String BLACK    = "\033[30m";
+  final String RED      = "\033[31m";
+  final String GREEN    = "\033[32m";
+  final String YELLOW   = "\033[33m";
+  final String BLUE     = "\033[34m";
+  final String PURPLE   = "\033[35m";
+  final String CYAN     = "\033[36m";
+  final String WHITE    = "\033[37m";
+  final String bBLACK   = "\033[40m";  //Virker ikke i Netbeans
+  final String bRED     = "\033[41m";
+  final String bGREEN   = "\033[42m";
+  final String bYELLOW  = "\033[43m";
+  final String bBLUE    = "\033[44m";
+  final String bPURPLE  = "\033[45m";
+  final String bCYAN    = "\033[46m";
+  final String bWHITE   = "\033[47m";
+
   public static String[][] onBoard = new String[8][8];
 
   public static void main(String[] args) {
     createBoard();
 
-    // System.out.println("Welcome to Ches(e)s");
-    // System.out.println("♔♕♖♗♘♙");
+    System.out.println("Welcome to Ches(e)s");
+
+    System.out.print(c.BLACK+""+c.bWHITE);
+    System.out.println("♔♕♖♗♘♙");
+
+    System.out.println(c.WHITE+""+c.bBLACK);
+    System.out.println("♔♕♖♗♘♙");
+
+    Print("#",8,1);
     // System.out.println("K Q T B H P");
     // System.out.println("♚♛♜ B♞♟");
     // System.out.println("k q t b h p{}");
@@ -53,21 +78,35 @@ public class Chess {
     Pawn bPawn6 = new Pawn(false);
     Pawn bPawn7 = new Pawn(false);
     Pawn bPawn8 = new Pawn(false);
+
+    while (wKing.alive || bKing.alive) {
+
+    }
+
   }
 
   static void displayBoard() {
-    System.out.println("A B C D E F G H  x");
+    System.out.println("\033[33mA B C D E F G H  x");
     for (int x = 0; x < 8; x++) {
       for (int y = 0; y < 8; y++) {
         if(onBoard[x][y]!=null) {
           System.out.print(onBoard[x][y]);
         } else if(y%2==x%2){
-          System.out.print("■ ");
+          System.out.print("\033[37m■ ");
         } else {
-          System.out.print("□ ");
+          System.out.print("\033[30m■ ");
         }
       }
-      System.out.println(" "+x);
+      System.out.println("\033[33m "+(x+1));
     }
   }
+}
+
+public void Print(String text, int f, int b) {
+  String y=(f+29)+"";
+  if(f==0||f>8){y="0";}
+  if(b>0&&b<9){y+=";"+(b+39);}
+  System.out.print("\033["+y+"m"+text);
+} public void Print() {
+    System.out.print(RESET);
 }
