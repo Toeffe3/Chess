0aeb49e31a078bb7ab94702f8b722ba3c551c011
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2019-01-24 13:43:53 +0100

    small fix

--- playchess/Piece.java
+++ playchess/Piece.java
@@ -8,6 +8,7 @@ abstract class Piece {
   int[] pos = {0,0};                                                  // The pieces position
 
   Piece(char color, char t, x, y) {                                   // Creates the piece
+  Piece(char color, char t, int x, int y) {                                   // Creates the piece
     player = color=='w'?true:false;                                   // Sets the player/color
     pos[0] = x; pos[1] = y;                                           // Set the position
     type = t=='k'?                                                    // Inline if else
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 REM Run the program in windows commandpromt
 REM Requires enviorment-path to javac to compile
-javac -d . -cp . playchess/*.java
+javac -d . -cp playchess/Chess playchess/*.java
 java playchess/Chess
